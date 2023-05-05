6a7f670e93e299a95fd36ae762d2b764d76e6114
khalidalshaibani <princeofimagination@hotmail.com>
2023-05-05 19:54:47 +0300

    Added summary for data used in this week, this month and last 30 days

--- NetworkWatcher.py
+++ NetworkWatcher.py
@@ -2,6 +2,7 @@
 # View the stats from the database
 
 import tkinter as tk
+from tkinter import ttk
 from tkinter import *
 from Table import Table
 from Stat import Stat
@@ -28,9 +29,23 @@ def main():
             },
             root
             ).table
-    
+    # create table for summary table
+    summaryTable = Table({
+                "Date": {'anchor':tk.CENTER,'width':220},
+                "Recieved": {'anchor':tk.CENTER,'width':100},
+                "Sent": {'anchor':tk.CENTER,'width':100},
+                "Total": {'anchor':tk.CENTER,'width':100},
+            },
+            {
+            },
+            root
+            ).table
+    style = ttk.Style()
+    style.configure("summaryStats.Treeview.Heading", font=('',1), background="none")
+    summaryTable.configure(style="summaryStats.Treeview", height=3)
+
     # create Stat object to deal with data
-    stat = Stat(root, dailyTable)
+    stat = Stat(root, dailyTable, summaryTable)
     # set the app icon and title
     img  = PhotoImage(file='/lib/NetworkWatcher/icon.png')
     root.title('Network Watcher v1.0')
@@ -38,7 +53,9 @@ def main():
 
     # fill data into the table
     stat.fillDailyTable()
+    stat.fillSummaryTable()
     dailyTable.pack(expand=True, fill='both')
+    summaryTable.pack(side="bottom")
     # start the window
     root.mainloop()
 
--- Stat.py
+++ Stat.py
@@ -1,14 +1,17 @@
 from DBConnection import conn
+import datetime
 
 class Stat:
     cursor = conn.cursor()
     REFRESH_TIMEOUT = 2000
     dailyTable = None
+    summaryTable = None
     root = None
 
-    def __init__(self, root, dailyTable):
+    def __init__(self, root, dailyTable, summaryTable):
         self.root = root
         self.dailyTable = dailyTable
+        self.summaryTable = summaryTable
 
     # convert data into a readable format
     def getDataAmount(self, data):
@@ -37,3 +40,26 @@ class Stat:
                 i+1, row[2], row[1], self.getDataAmount(row[4]), self.getDataAmount(row[3]), self.getDataAmount(row[5])))
         # keep doing this again and again
         self.root.after(self.REFRESH_TIMEOUT, self.fillDailyTable)
+
+    # fill the summary stats table from the database
+    def fillSummaryTable(self):
+        today = datetime.date.today()
+        #  get start dates
+        dates = {
+            'This Week': today - datetime.timedelta(days=today.weekday()),
+            'This Month': today.replace(day=1),
+            'Last 30 Days': today - datetime.timedelta(days=30)
+        }
+
+        # clear all existing data from the table
+        self.summaryTable.delete(*self.summaryTable.get_children())
+
+        # fill new data
+        for date, value in dates.items():
+            self.cursor.execute(f"SELECT SUM(sent), SUM(recieved), SUM(usage) FROM dailyStats WHERE date >= '{value}'")
+            row = self.cursor.fetchone()
+            self.summaryTable.insert(parent='', index='end', iid=None, values=(
+                date, self.getDataAmount(row[0]), self.getDataAmount(row[1]), self.getDataAmount(row[2])))
+
+        # keep doing this again and again
+        self.root.after(self.REFRESH_TIMEOUT, self.fillSummaryTable)
